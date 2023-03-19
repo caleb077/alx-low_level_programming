903cebc11f44211f00bb45839a80a8fc30c13ec1
myusername <myemail@gmail.com>
2023-03-19 08:16:02 -0700

    C - Variables, if, else, while

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,23 +6,23 @@
  * main - Entry point
  *
  * Return: Always 0 (Success)
- */
-
+*/
 int main(void)
 {
-	int d;
+	int num;
 
-	for (d = 0; d < 100; d++)
+	for (num = 0; num <= 9; num++)
 	{
-		putchar((d / 10) + '0');
-		putchar((d % 10) + '0');
-		if (d != 99)
-		{
-			putchar(',');
-			putchar(' ');
-		}
+		putchar((num % 10) + '0');
+		if (num == 9)
+			continue;
+
+		putchar(',');
+		putchar(' ');
 	}
+
 	putchar('\n');
 
 	return (0);
 }
+
