f043e080e115e66401b1c68ebadafc7f95b9f154
root <kaleabsisay2698@gmail.com>
2023-03-23 13:11:13 -0700

    C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,31 +1,36 @@
-#include <main.h>
+#include "main.h"
+void print_integer(int m);
 
 /**
- * main - Finds and prints the largest prime
- *        factor of the number 612852475143.
- *
- * Return: Always 0.
+ * print_number - a function that prints an integer.
+ * @n: An input integer
+ * Return: Nothing
  */
-int main(void)
+void print_number(int n)
 {
-	long prime = 612852475143, div;
-
-	while (div < (prime / 2))
+	if (n == 0)
+		_putchar('0');
+	else if (n < 0)
 	{
-		if ((prime % 2) == 0)
-		{
-			prime /= 2;
-			continue;
-		}
-
-		for (div = 3; div < (prime / 2); div += 2)
-		{
-			if ((prime % div) == 0)
-				prime /= div;
-		}
+		_putchar('-');
+		print_integer(n * -1);
 	}
+	else
+		print_integer(n);
+}
 
-	printf("%ld\n", prime);
+/**
+ * print_integer - A function to priting n
+ * @m: an input unsigned integer
+ * Return: Nothing
+ */
+void print_integer(int m)
+{
+	int i = 1000000000;
 
-	return (0);
+	for (; i >= 1; i /= 10)
+		if (m / i != 0)
+		{
+			_putchar((m / i) % 10 + '0');
+		}
 }
