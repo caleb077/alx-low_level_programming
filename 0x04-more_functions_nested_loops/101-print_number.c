584baa82a895146a2aee83d84274a374b8784ee4
root <kaleabsisay2698@gmail.com>
2023-03-24 11:19:46 -0700

    C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,12 +6,12 @@ void print_integer(int n);
  * @n: An input integer
  * Return:  Always 0
  */
+
 void print_number(int n)
 {
 	if (n == 0)
 		_putchar('0');
-	else
-	       	if (n < 0)
+	else if (n < 0)
 	{
 		_putchar('-');
 		print_integer(n * -1);
