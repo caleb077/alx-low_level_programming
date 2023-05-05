a1f6317366e7263413d00a5b208d515463198b38
root <kaleabsisay2698@gmail.com>
2023-05-05 08:12:25 -0700

    C - Bit manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,23 +8,18 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int base = 1, result = 0, len = 0;
+    int i;
+    unsigned int dec_val = 0;
 
-	if (b == NULL)
-		return (0);
+    if (!b)
+        return (0);
 
-	while (b[len])
-		len++;
+    for (i = 0; b[i]; i++)
+    {
+        if (b[i] < '0' || b[i] > '1')
+            return (0);
+        dec_val = 2 * dec_val + (b[i] - '0');
+    }
 
-	while (len)
-	{
-		if (b[len - 1] != '0' && b[len - 1] != '1')
-			return (0);
-
-		if (b[len - 1] == '1')
-			result += base;
-		base *= 2;
-		len--;
-	}
-	return (result);
+    return (dec_val);
 }
